f1ce458aed88dd0077cfc6a5c855a91e1c33e7b5
PtFux <91057700+PtFux@users.noreply.github.com>
2024-09-24 23:15:04 +0300

    added parse and validation in AuthLoginPost

--- internal/auth/api/api_auth.go
+++ internal/auth/api/api_auth.go
@@ -10,13 +10,34 @@ import (
 
 // AuthLoginPost - ручка аутентификации
 func AuthLoginPost(w http.ResponseWriter, r *http.Request) {
+	op := "auth.api.api_auth.AuthLoginPost"
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+
+	// parse models
+	var l models.Login
+	if err := json.NewDecoder(r.Body).Decode(&l); err != nil {
+		slog.Info(fmt.Sprintf("error {%v} | in %v", err, op))
+		// TODO: Дописать отправку модели ошибки с err.msg
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	// validate fields
+	if _, errV := l.Validate(); errV != nil {
+		slog.Info(fmt.Sprintf("error {%v} | in %v %v", errV, op, errV.GetMsg()))
+
+		// TODO: Дописать отправку модели ошибки с err.msg
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	slog.Info(fmt.Sprintf("valid request | in %v", op))
 	w.WriteHeader(http.StatusOK)
 }
 
 // AuthRegisterPost - ручка регистрации
 func AuthRegisterPost(w http.ResponseWriter, r *http.Request) {
-	op := "auth.api.api_auth"
+	op := "auth.api.api_auth.AuthRegisterPost"
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 
 	// parse models
