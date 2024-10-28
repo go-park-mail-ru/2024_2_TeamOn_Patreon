014cc8a0e064a59aa9a207642ad32cf7de8632c1
PtFux <polina.tikunowa@gmail.com>
2024-10-28 14:07:32 +0300

    polina: db: added check constraint

--- database/migrations/13_create_custom_subscription.up.sql
+++ database/migrations/13_create_custom_subscription.up.sql
@@ -2,7 +2,7 @@ CREATE TABLE custom_subscription (
     custom_subscription_id uuid PRIMARY KEY,
     author_id uuid REFERENCES people (user_id) ON DELETE CASCADE NOT NULL, -- без автора нет подписки
     custom_name text NOT NULL,  -- имя кастомной подписки должно быть
-    cost integer NOT NULL,      -- стоимость должна быть
+    cost integer NOT NULL check ( cost > 0 ),      -- стоимость должна быть
     subscription_layer_id uuid REFERENCES subscription_layer ON DELETE RESTRICT NOT NULL, -- уровень подписки должен быть
     created_date timestamp NOT NULL DEFAULT now(),
     UNIQUE (author_id, custom_name),
