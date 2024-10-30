3386f8728a44a6603d0f3f5d89f68532cf56afcf
PtFux <polina.tikunowa@gmail.com>
2024-10-30 12:42:14 +0300

    polina: posts: controller: added PostPostIdDelete

--- internal/content/controller/post_post_id_delete.go
+++ internal/content/controller/post_post_id_delete.go
@@ -1,57 +1,55 @@
 package controller
 
 import (
-	"encoding/json"
 	tModels "github.com/go-park-mail-ru/2024_2_TeamOn_Patreon/internal/content/controller/models"
-	"github.com/go-park-mail-ru/2024_2_TeamOn_Patreon/internal/content/controller/models/mapper"
 	"github.com/go-park-mail-ru/2024_2_TeamOn_Patreon/internal/pkg/global"
 	"github.com/go-park-mail-ru/2024_2_TeamOn_Patreon/internal/pkg/logger"
+	bModels "github.com/go-park-mail-ru/2024_2_TeamOn_Patreon/internal/pkg/service/models"
 	"github.com/go-park-mail-ru/2024_2_TeamOn_Patreon/internal/pkg/utils"
+	"github.com/gorilla/mux"
 	"net/http"
-	"strconv"
 )
 
 func (h Handler) PostsPostIdDelete(w http.ResponseWriter, r *http.Request) {
 	op := "content.controller.post_post_id_delete.PostsPostIdDelete"
-
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 
-	// Получение параметров `offset` и `limit` из запроса
-	offsetStr := r.URL.Query().Get("offset")
-	limitStr := r.URL.Query().Get("limit")
-
-	// Установка значений по умолчанию
-	offset := 0
-	limit := 10
-
-	// Преобразование `offset` и `limit` в целые числа
-	if offsetStr != "" {
-		if o, err := strconv.Atoi(offsetStr); err == nil {
-			offset = o
-		}
-	}
-	if limitStr != "" {
-		if l, err := strconv.Atoi(limitStr); err == nil {
-			limit = l
-		}
-	}
-
-	posts, err := h.b.GetPopularPosts(offset, limit)
-	if err != nil {
+	// Достаем postId
+	vr := mux.Vars(r)
+	postId, ok := vr["postId"]
+	if !ok {
+		// Надо бэд реквест
+		err := global.ErrServer
+		logger.StandardWarnF(op, "Received validation error={%v}", err)
+		// проставляем http.StatusBadRequest
 		logger.StandardResponse(err.Error(), global.GetCodeError(err), r.Host, op)
 		w.WriteHeader(global.GetCodeError(err))
+		// отправляем структуру ошибки
 		utils.SendStringModel(&tModels.ModelError{Message: global.GetMsgError(err)}, w, op)
 		return
 	}
 
-	popularPosts := make([]*tModels.Post, 0, len(posts))
-	for _, post := range posts {
-		popularPosts = append(popularPosts, mapper.MapInterfacePostToTransportPost(post))
+	// Достаем юзера
+	user, ok := r.Context().Value(global.UserKey).(bModels.User)
+	if !ok {
+		// проставляем http.StatusUnauthorized 401
+		logger.StandardResponse("userData not found in context", http.StatusUnauthorized, r.Host, op)
+		w.WriteHeader(http.StatusUnauthorized)
+		return
 	}
 
-	w.WriteHeader(http.StatusOK)
-	if err = json.NewEncoder(w).Encode(popularPosts); err != nil {
+	// Удаляем пост
+	userId := user.UserID
+	err := h.b.DeletePost(string(userId), postId)
+	if err != nil {
+		logger.StandardWarnF(op, "Received validation error={%v}", err)
+		// проставляем http.StatusBadRequest
 		logger.StandardResponse(err.Error(), global.GetCodeError(err), r.Host, op)
 		w.WriteHeader(global.GetCodeError(err))
+		// отправляем структуру ошибки
+		utils.SendStringModel(&tModels.ModelError{Message: global.GetMsgError(err)}, w, op)
+		return
 	}
+
+	w.WriteHeader(http.StatusNoContent)
 }
