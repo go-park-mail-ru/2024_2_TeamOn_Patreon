ec6a82ebbe0aa2eb6a6b8849035378dc03168ece
PtFux <91057700+PtFux@users.noreply.github.com>
2024-11-06 14:58:19 +0300

    logger: added csrf logging

--- internal/pkg/middlewares/csrf_middleware.go
+++ internal/pkg/middlewares/csrf_middleware.go
@@ -3,10 +3,11 @@ package middlewares
 import (
 	"crypto/rand"
 	"encoding/base64"
+	"github.com/go-park-mail-ru/2024_2_TeamOn_Patreon/internal/pkg/logger"
 	"net/http"
 )
 
-const csrfTokenCookieName = "csrf_token_yd23g8763dfg28cg2e3iuy"
+const csrfTokenCookieName = "csrf_token"
 
 // generateCSRFToken создает случайный токен для защиты от CSRF
 func generateCSRFToken() (string, error) {
@@ -20,12 +21,15 @@ func generateCSRFToken() (string, error) {
 
 // CsrfMiddleware для проверки CSRF токена
 func CsrfMiddleware(next http.Handler) http.Handler {
+	op := "CsrfMiddleware"
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if r.Method == http.MethodPost || r.Method == http.MethodPut || r.Method == http.MethodDelete {
 			// Извлекаем CSRF токен из заголовка запроса
 			csrfToken := r.Header.Get("X-CSRF-Token")
 			// Получаем токен из cookie
 			cookie, err := r.Cookie(csrfTokenCookieName)
+			logger.StandardDebugF(r.Context(), op, "CSRF from headers=%v", csrfToken)
+			logger.StandardDebugF(r.Context(), op, "CSRF from cookie=%v", cookie)
 			if err != nil || csrfToken != cookie.Value {
 				http.Error(w, "CSRF token mismatch", http.StatusForbidden)
 				return
