bb784fc5344255247aa2d7a318d6030c22f428ed
PtFux <91057700+PtFux@users.noreply.github.com>
2024-11-07 13:57:22 +0300

    database: subscribe: added constraint

--- database/migrations/14_create_subscription.up.sql
+++ database/migrations/14_create_subscription.up.sql
@@ -3,5 +3,6 @@ CREATE TABLE subscription (
     user_id uuid REFERENCES people ON DELETE CASCADE NOT NULL,       -- без юзера нет подписки
     custom_subscription_id uuid REFERENCES custom_subscription ON DELETE CASCADE NOT NULL, -- без подписки нет подписки
     started_date timestamp NOT NULL DEFAULT now(),
-    finished_date timestamp NOT NULL
+    finished_date timestamp NOT NULL,
+    UNIQUE (user_id, custom_subscription_id)
 );
