f9d9bc9c561f1953a5e1b75bbe40845e6baab168
PtFux <91057700+PtFux@users.noreply.github.com>
2024-11-19 20:17:23 +0300

    content: repository: corrected feed sql

--- internal/content/repository/postgresql/content.go
+++ internal/content/repository/postgresql/content.go
@@ -1,10 +1,1 @@
 package postgresql
-
-//
-//func (cr *ContentRepository) UpdatePostContent() error {
-//	return nil
-//}
-//
-//func (cr *ContentRepository) InsertPostContent() error {
-//	return nil
-//}
