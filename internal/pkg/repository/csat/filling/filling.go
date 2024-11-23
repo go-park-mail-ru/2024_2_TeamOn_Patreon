c5ae06015aeeecf03ae4d2a333a1e576a2c32679
ptfux <polina.tikunowa@gmail.com>
2024-11-23 17:39:39 +0300

    csat:  added filling

--- internal/pkg/repository/csat/filling/filling.go
+++ internal/pkg/repository/csat/filling/filling.go
@@ -12,8 +12,6 @@ import (
 
 func main() {
 	// Укажите, сколько пользователей нужно создать
-	nType := consts.QuestionTypeCount
-	nQuestions := consts.QuestionForTypeCount
 
 	// Параметры подключения к PostgreSQL
 	dbHost := "postgres-csat"
@@ -34,22 +32,24 @@ func main() {
 	}
 	defer pool.Close()
 
-	if err := createQuestionType(context.Background(), pool, nType); err != nil {
+	metaQuestion := map[string][]string{consts.QuestionType0: {consts.Question_0_1, consts.Question_0_2, consts.Question_0_3, consts.Question_0_4, consts.Question_0_5, consts.Question_0_6, consts.Question_0_7, consts.Question_0_8, consts.Question_0_9, consts.Question_0_10, consts.Question_11}, consts.QuestionType1: {consts.Question_1_1, consts.Question_1_2, consts.Question_1_3, consts.Question_1_4, consts.Question_1_5, consts.Question_1_6, consts.Question_1_7, consts.Question_1_8, consts.Question_1_9, consts.Question_1_10}, consts.QuestionType2: {consts.Question_2_1, consts.Question_2_2, consts.Question_2_3, consts.Question_2_4, consts.Question_2_5, consts.Question_2_6, consts.Question_2_7, consts.Question_2_8, consts.Question_2_9, consts.Question_2_10}}
+
+	if err := createQuestionType(context.Background(), pool, metaQuestion); err != nil {
 		log.Fatalf("Ошибка при создании типов вопросов: %v", err)
 	}
 
-	log.Printf("Создано типов %v", nType)
+	log.Printf("Создано типов %v", len(metaQuestion))
 
-	if err := createQuestion(context.Background(), pool, nQuestions*nType); err != nil {
+	if err := createQuestion(context.Background(), pool, metaQuestion); err != nil {
 		log.Fatalf("Ошибка при создании вопросов %v", err)
 	}
-	log.Printf("Создано вопросов %v", nQuestions)
+	log.Printf("Создано вопросов %v", metaQuestion)
 
 	log.Printf("dbUser: %v dbHost=%v dbPort=%v dbName=%v", dbUser, dbHost, dbPort, dbName)
 
 }
 
-func createQuestionType(ctx context.Context, pool *pgxpool.Pool, n int) error {
+func createQuestionType(ctx context.Context, pool *pgxpool.Pool, meta map[string][]string) error {
 	conn, err := pool.Acquire(ctx)
 	if err != nil {
 		return fmt.Errorf("не удалось получить соединение из пула: %v", err)
@@ -59,16 +59,14 @@ func createQuestionType(ctx context.Context, pool *pgxpool.Pool, n int) error {
 	batch := &pgx.Batch{}
 
 	// Подготавливаем данные для пользователей и связанных записей
-	for i := 0; i < n; i++ {
+	for qType, _ := range meta {
 		questionTypeID := uuid.NewV4()
-		questionType := consts.QuestionType
 
 		// Запрос на добавление пользователя
 		batch.Queue(`
             INSERT INTO question_theme (question_theme_id, theme) 
             VALUES ($1, $2)`,
-			questionTypeID, questionType)
-
+			questionTypeID, qType)
 	}
 
 	// Выполнение батч-запроса
@@ -86,7 +84,7 @@ func createQuestionType(ctx context.Context, pool *pgxpool.Pool, n int) error {
 	return nil
 }
 
-func createQuestion(ctx context.Context, pool *pgxpool.Pool, n int) error {
+func createQuestion(ctx context.Context, pool *pgxpool.Pool, meta map[string][]string) error {
 	conn, err := pool.Acquire(ctx)
 	if err != nil {
 		return fmt.Errorf("не удалось получить соединение из пула: %v", err)
@@ -96,16 +94,16 @@ func createQuestion(ctx context.Context, pool *pgxpool.Pool, n int) error {
 	batch := &pgx.Batch{}
 
 	// Подготавливаем данные для пользователей и связанных записей
-	for i := 0; i < n; i++ {
-		questionID := uuid.NewV4()
-		questionType := consts.QuestionType
-		question := consts.Question
+	for qType, questions := range meta {
+		for _, question := range questions {
+			questionID := uuid.NewV4()
 
-		// Запрос на добавление пользователя
-		batch.Queue(`
+			// Запрос на добавление пользователя
+			batch.Queue(`
             INSERT INTO question (question_id, question_theme_id, question) 
             VALUES ($1, (select question_theme_id from question_theme where theme= $2  limit 1), $3)`,
-			questionID, questionType, question)
+				questionID, qType, question)
+		}
 
 	}
 
